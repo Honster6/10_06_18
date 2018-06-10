3d61279cbc2e107949626d55984087333b7a919f
Sebastian <40136322+Honster6@users.noreply.github.com>
2018-06-10 14:06:44 +0200

    dodanie halo

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("Hello World");
         System.out.println("Dzien Dobry!");
         System.out.println("Guten Tag");
+        System.out.println("Halo");
     }
 }
